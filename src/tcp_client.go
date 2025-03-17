1dd2d5855db6ee204f29afa042810d5b8afb077f
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-03-17 15:08:11 +0200

    create tcp client for get request on port 80

--- src/tcp_client.go
+++ src/tcp_client.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"crypto/tls"
 	"fmt"
+	"net"
 	"net/url"
 )
 
@@ -19,9 +20,14 @@ func Fetch(urlStr string) (string, error) {
 		path = "/"
 	}
 
-	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:443", host), &tls.Config{})
+	var conn net.Conn
+	conn, err = fetchHttps(host)
+
 	if err != nil {
-		return "", err
+		conn, err = fetchHttp(host)
+		if err != nil {
+			return "", fmt.Errorf("failed to connect to %s (HTTPS/HTTP): %v", urlStr, err)
+		}
 	}
 	defer conn.Close()
 
@@ -29,7 +35,7 @@ func Fetch(urlStr string) (string, error) {
 		"GET %s HTTP/1.1\r\n"+
 			"Host: %s\r\n"+
 			"Connection: close\r\n"+
-			"User-Agent: : Mozilla/5.0\r\n\r\n", path, host)
+			"User-Agent: Mozilla/5.0\r\n\r\n", path, host)
 
 	_, err = conn.Write([]byte(request))
 	if err != nil {
@@ -50,6 +56,30 @@ func Fetch(urlStr string) (string, error) {
 		}
 	}
 
-	return response, nil
+	if err := scanner.Err(); err != nil {
+		return "", fmt.Errorf("error reading response: %v", err)
+	}
 
+	return response, nil
+}
+
+func fetchHttps(host string) (*tls.Conn, error) {
+	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:443", host), &tls.Config{})
+	if err != nil {
+		fmt.Printf("failed to connect via HTTPS: %v", err)
+		return nil, fmt.Errorf("failed to connect via HTTPS: %v", err)
+	}
+	fmt.Printf("\n\nConnection via HTTPS succesful\n\n")
+	return conn, nil
+}
+
+func fetchHttp(host string) (net.Conn, error) {
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:80", host))
+	if err != nil {
+		fmt.Printf("failed to connect via HTTP: %v", err)
+		return nil, fmt.Errorf("failed to connect via HTTP: %v", err)
+	}
+	fmt.Printf("\n\nConnection via HTTP succesful\n\n")
+
+	return conn, nil
 }
